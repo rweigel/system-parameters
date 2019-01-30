62456c2c6758312439732044c5f8e0c2fc6400c8
weigel <rweigel@gmu.edu>
2019-01-30 12:09:42 -0500

    update figs

--- figures/update.sh
+++ figures/update.sh
@@ -1,3 +1,4 @@
+git pull
 DIR=../../figures/combined
 cp $DIR/plot_TF_aves_aobo_histograms-options-1.pdf .
 cp $DIR/plot_TF_aves_H-options-1.pdf .
@@ -6,3 +7,5 @@ cp $DIR/plot_TF_aves_Z-options-1.pdf .
 cp $DIR/plot_TF_aves_SN-options-1.pdf .
 
 cp $DIR/../20060402/plot_raw_All_20060402.pdf .
+git commit -a -m "update figs"
+git push
