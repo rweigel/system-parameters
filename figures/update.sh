002d8b2160a9812094cc6ece88b19d94d9a610cf
weigel <rweigel@gmu.edu>
2019-01-30 09:51:27 -0500

    SN plot

--- figures/update.sh
+++ figures/update.sh
@@ -3,5 +3,6 @@ cp $DIR/plot_TF_aves_aobo_histograms-options-1.pdf .
 cp $DIR/plot_TF_aves_H-options-1.pdf .
 cp $DIR/plot_TF_aves_Phi-options-1.pdf .
 cp $DIR/plot_TF_aves_Z-options-1.pdf .
+cp $DIR/plot_TF_aves_SN-options-1.pdf .
 
 cp $DIR/../20060402/plot_raw_All_20060402.pdf .
