f45d771dd1093d2352f51c05d76b4d92295a2594
weigel <rweigel@gmu.edu>
2019-01-30 09:17:18 -0500

    Update figs

--- figures/update.sh
+++ figures/update.sh
@@ -1,7 +1,7 @@
 DIR=../../figures/combined
-cp $DIR/plot_TF_aves_aobo_histograms-none-none-rectangular-parzen-ols_on_Z.pdf .
-cp $DIR/plot_TF_aves_GE_H-none-none-rectangular-parzen-ols_on_Z.pdf .
-cp $DIR/plot_TF_aves_GE_Phi-none-none-rectangular-parzen-ols_on_Z.pdf .
-cp $DIR/plot_TF_aves_GE_Z-none-none-rectangular-parzen-ols_on_Z.pdf .
+cp $DIR/plot_TF_aves_aobo_histograms-options-1.pdf .
+cp $DIR/plot_TF_aves_H-options-1.pdf .
+cp $DIR/plot_TF_aves_Phi-options-1.pdf .
+cp $DIR/plot_TF_aves_Z-options-1.pdf .
 
 cp $DIR/../20060402/plot_raw_All_20060402.pdf .
