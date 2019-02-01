114f56ae6e1a28f77b3ab15b331c4c3e6d88709f
weigel <rweigel@gmu.edu>
2019-02-01 17:13:37 -0500

    update figs

--- figures/update.sh
+++ figures/update.sh
@@ -9,6 +9,6 @@ cp $DIR/plot_TF_aves_Phi-options-1.pdf .
 cp $DIR/plot_TF_aves_Z-options-1.pdf .
 cp $DIR/plot_TF_aves_SN-options-1.pdf .
 
-cp $DIR/../20060402/plot_raw_All_20060402.pdf .
+cp $DIR/../20080805/plot_raw_All_20060805.pdf .
 git commit -a -m "update figs"
 git push
