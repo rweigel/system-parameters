c039a84857ddcc8f45926d431bc5d124f09ee32a
weigel <rweigel@gmu.edu>
2019-02-11 08:43:35 -0500

    update figs

--- figures/update.sh
+++ figures/update.sh
@@ -3,11 +3,11 @@ git pull
 cp ../../figures/20060805/plot_model_predictions-MeanModel-20060806T150000.pdf .
 
 DIR=../../figures/combined
-cp $DIR/plot_TF_aves_aobo_histograms-options-1.pdf .
-cp $DIR/plot_TF_aves_H-options-1.pdf .
-cp $DIR/plot_TF_aves_Phi-options-1.pdf .
-cp $DIR/plot_TF_aves_Z-options-1.pdf .
-cp $DIR/plot_TF_aves_SN-options-1.pdf .
+cp $DIR/main_plot_summary_aobo_histograms-options-1.pdf .
+cp $DIR/main_plot_summary_H-options-1.pdf .
+cp $DIR/main_plot_summary_Phi-options-1.pdf .
+cp $DIR/main_plot_summary_Z-options-1.pdf .
+cp $DIR/main_plot_summary_SN-options-1.pdf .
 
 cp $DIR/../20060805/plot_raw_All_20060805.pdf .
 git commit -a -m "update figs"
