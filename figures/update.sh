9015dba895f951b0d04180cfdbf1f85e7893179a
weigel <rweigel@gmu.edu>
2019-02-23 14:38:21 -0500

    update figs

--- figures/update.sh
+++ figures/update.sh
@@ -8,6 +8,10 @@ cp $DIR/plot_model_summary_H-options-1.pdf .
 cp $DIR/plot_model_summary_Phi-options-1.pdf .
 cp $DIR/plot_model_summary_Z-options-1.pdf .
 cp $DIR/plot_model_summary_SN-options-1.pdf .
+cp $DIR/plot_options_comparison_a.pdf .
+cp $DIR/plot_options_comparison_b.pdf .
+cp $DIR/plot_options_comparison_zx.pdf .
+cp $DIR/plot_options_comparison_zy.pdf .
 
 cp $DIR/../20060805/plot_raw_All_20060805.pdf .
 git commit -a -m "update figs"
