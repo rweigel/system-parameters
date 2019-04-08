600676c810714db16ca845efd3b533e6798fb326
weigel <rweigel@gmu.edu>
2019-04-08 16:01:25 -0400

    update figs

--- figures/update.sh
+++ figures/update.sh
@@ -4,14 +4,16 @@ cp ../../figures/predictions/plot_model_predictions-MeanModel-2006-08-06.pdf .
 
 DIR=../../figures/combined
 cp $DIR/plot_model_summary_aobo_histograms-options-1.pdf .
-cp $DIR/plot_model_summary_H-options-1.pdf .
+#cp $DIR/plot_model_summary_H-options-1.pdf .
 cp $DIR/plot_model_summary_Phi-options-1.pdf .
 cp $DIR/plot_model_summary_Z-options-1.pdf .
+cp $DIR/plot_model_summary_Phi_MT-options-1.pdf .
+cp $DIR/plot_model_summary_Z_MT-options-1.pdf .
 cp $DIR/plot_model_summary_SN-options-1.pdf .
-cp $DIR/plot_options_comparison_a.pdf .
-cp $DIR/plot_options_comparison_b.pdf .
-cp $DIR/plot_options_comparison_zx.pdf .
-cp $DIR/plot_options_comparison_zy.pdf .
+#cp $DIR/plot_options_comparison_a.pdf .
+#cp $DIR/plot_options_comparison_b.pdf .
+#cp $DIR/plot_options_comparison_zx.pdf .
+#cp $DIR/plot_options_comparison_zy.pdf .
 
 cp $DIR/../20060805/plot_raw_All_20060805.pdf .
 git commit -a -m "update figs"
