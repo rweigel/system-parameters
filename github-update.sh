00cad93d4ddaebf0c4f8d20daef83daa7b874473
Robert Weigel <rweigel@gmu.edu>
2019-07-17 12:38:35 -0400

    update figs

--- github-update.sh
+++ github-update.sh
@@ -1,19 +1,17 @@
 git pull
 
-cd figures
-
 DIR=../../figures/combined
 
-cp ../../map/map.pdf .
-cp $DIR/../20060805/plot_raw_All_20060805.pdf .
-cp ../../figures/predictions/plot_model_predictions-MeanModel-2006-08-06.pdf .
+cp ../../map/map.pdf figures
+cp $DIR/../20060805/plot_raw_All_20060805.pdf figures
+cp ../../figures/predictions/plot_model_predictions-MeanModel-2006-08-06.pdf figures
 
-cp $DIR/plot_model_summary_aobo_histograms-options-1.pdf .
-cp $DIR/plot_model_summary_Phi-options-1.pdf .
-cp $DIR/plot_model_summary_Z-options-1.pdf .
-cp $DIR/plot_model_summary_Phi_MT-options-1.pdf .
-cp $DIR/plot_model_summary_Z_MT-options-1.pdf .
-cp $DIR/plot_model_summary_SN-options-1.pdf .
+cp $DIR/plot_model_summary_aobo_histograms-options-1.pdf figures
+cp $DIR/plot_model_summary_Phi-options-1.pdf figures
+cp $DIR/plot_model_summary_Z-options-1.pdf figures
+cp $DIR/plot_model_summary_Phi_MT-options-1.pdf figures
+cp $DIR/plot_model_summary_Z_MT-options-1.pdf figures
+cp $DIR/plot_model_summary_SN-options-1.pdf figures
 
 
 git commit -a -m "update figs"
