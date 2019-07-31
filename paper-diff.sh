20dae8621e64fd0e789fad875d35dd709aef772f
Robert Weigel <rweigel@gmu.edu>
2019-07-31 16:24:35 -0400

    Update for filename change

--- paper-diff.sh
+++ paper-diff.sh
@@ -1,1 +1,1 @@
-latexdiff paper-submitted-1.tex paper.tex > paper-diff.tex; git commit paper-diff.tex -m "Update diff"; git push
+latexdiff paper-submitted-1.tex paper-submitted-2.tex > paper-diff.tex; git commit paper-diff.tex -m "Update diff"; git push
