1872396c90c58ecc28e4719bca33c745a2280c29
weigel <rweigel@gmu.edu>
2019-07-30 21:25:36 -0400

    new figs

---
+++ figures/update.sh
@@ -0,0 +1,15 @@
+git pull
+
+DIR=../../../figures
+
+cp $DIR/20060805/plot_raw_All_20060805-v1.pdf .
+cp $DIR/predictions/plot_GIC_predictions-MeanModel-2006-08-06-v0-o0.pdf .
+cp $DIR/summary/plot_model_summary_SN-options-1-v0-o0.pdf .
+cp $DIR/summary/plot_model_summary_aobo_histograms-options-1-v0-o0.pdf .
+cp $DIR/summary/plot_model_summary_Z-options-1-v0-o0.pdf .
+cp $DIR/summary/plot_model_summary_Phi-options-1-v1-o0.pdf .
+cp $DIR/summary/plot_model_summary_Phi_MT-options-1-v1-o0.pdf .
+cp $DIR/summary/plot_model_summary_Z_MT-options-1-v0-o0.pdf .
+
+#git commit -a -m "update figs"
+#git push
